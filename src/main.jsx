cbd1ddd0f86c5e6cb3411a2601a7e04e58279600
Rashid Ali <l1f21bsse0089@ucp.edu.pk>
2025-06-27 01:32:50 +0500

    Fix white screen by temporarily disabling Stripe integration

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
 import { LanguageProvider } from "./context/LanguageContext";
 import { AuthProvider } from "./context/AuthContext";
-
-const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY || "pk_test_123");
+import ErrorBoundary from "./ErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Elements stripe={stripePromise}>
+  <ErrorBoundary>
     <LanguageProvider>
       <AuthProvider>
         <BrowserRouter>
@@ -19,5 +16,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         </BrowserRouter>
       </AuthProvider>
     </LanguageProvider>
-  </Elements>
+  </ErrorBoundary>
 );
